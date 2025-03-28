68bf8aac628060702d0e7f8a9dd6f6d17cd159f1
EduSS28 <34982059+EduSS282@users.noreply.github.com>
2025-03-28 18:19:16 +0100

    Add files via upload

---
+++ Test8.asm
@@ -0,0 +1,23 @@
+lw r1, 20(r0)
+add r1,r2,r1
+ret r1
+nop
+nop
+add r0,r0,r0
+nop
+jal r30, 0
+jal r30, 2
+nop
+nop
+add r1,r1,r1
+jal r30, 1
+add r30,r30,r30
+add r30,r30,r30
+beq r30,r30, 2
+nop
+nop
+add r2,r2,r2
+add r1,r1,r1
+add r3,r1,r2
+beq r3,r1,-1
+beq r0,r0,-1
